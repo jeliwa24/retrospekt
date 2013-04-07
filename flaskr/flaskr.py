b77528ed263f663ff3ead5bdd03ad9f89665664e
Lucy <peachie.monkey@gmail.com>
2013-04-07 04:14:51 -0400

    Added pie chart, summarized data, edited CSS file to look pretty

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,7 +1,8 @@
 from __future__ import with_statement
 from contextlib import closing
 # all the imports
-import sqlite3
+import sqlite3, json, urllib, pprint
+import pdb
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
@@ -36,9 +37,31 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+    #cur = g.db.execute('select title, text from entries order by id desc')
+    #entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    url = "https://api.foursquare.com/v2/users/self/checkins?oauth_token=BIX22BVXBSLL32SXQCO1SCMQSRZDEPBY0W3IDGWSUK1BV5NS&v=20130407"
+    output = json.load(urllib.urlopen(url))
+    
+    for i in output['response']['checkins']['items']:
+        cat = i['venue']['categories'][0]['name']
+        tmp = g.db.execute ('select category, count from entries where category=(?)', [cat])
+        if len(tmp.fetchall())!=0:
+            g.db.execute ('update entries set count=count+1 where category=(?)', [cat])
+            print 'hi'
+            g.db.commit()
+        else: 
+            g.db.execute ('insert into entries (category, count) values (?, ?)', [cat, 1])
+            g.db.commit()
+    
+    
+    cur = g.db.execute('select category, count from entries order by id desc')
+    entries = [dict(category=row[0], count=row[1]) for row in cur.fetchall()]
+
+    count = len(output['response']['checkins']['items'])
+    #pdb.set_trace()
+    test = [dict(place=i['venue']['name']) for i in output['response']['checkins']['items']]
+    return render_template('show_entries.html', entries=entries, test=test, count=count)
+
 
 @app.route('/add', methods=['POST'])
 def add_entry():
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,6 +1,6 @@
 drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
-  title string not null,
-  text string not null
+  category string not null,
+  count integer not null
 );
